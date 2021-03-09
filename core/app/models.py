d0cb41f4a113f0d6389afddbb01e0a9ee856c660
abhishek <abhismalviya@outlook.com>
2021-03-09 18:39:08 +0530

    sign up form

--- core/app/models.py
+++ core/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    fname = models.CharField(max_length = 50)
+    lname = models.CharField(max_length = 50)
+    mail = models.EmailField(max_length = 254, unique = True)
+    joinDate = models.DateField(auto_now = True)
+
+    def __str__(self):
+        return self.fname + ' ' + self.lname
--- core/app/views.py
+++ core/app/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import ContactForm
+from .models import Contact
+
 # Create your views here.
 
 app_name = 'app'
 
 def signUp(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            contact = Contact.objects.create(fname = data['fname'],lname = data['lname'], mail = data['mail'])
+            contact.save()
+            return HttpResponse('ThankYou')
+    else:
+        form = ContactForm()
+
     ctx = {
         'title':'SignUp',
-        'heading':'SignUp'
+        'form': form
         }
-    return render(request, './app/base.html'  , ctx)
+
+    return render(request, './app/signup.html'  , ctx )
